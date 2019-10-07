fe43437fc9ea00012deba53a0b4af8bfa628279f
Walter Johnson <johnwalt@oregonstate.edu>
2019-10-06 21:00:57 -0700

    files for add and multiply

---
+++ matrixAdd.sh
@@ -0,0 +1,5 @@
+# matrixAdd.sh
+#!/bin/bash
+
+# take two MxN matrices and add them together element-wise to produce and MxN matrix.
+# should return and error of the matrices do not have the same dimensions
---
+++ matrixMultiply.sh
@@ -0,0 +1,5 @@
+# matrixMultiply.sh
+#!/bin/bash
+
+# take an MxN and an NxP matrix and produce an MxP matrix.
+# note that matrix multiplication is not commutative.
